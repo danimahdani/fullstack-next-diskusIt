803ac95db766b174a31b0c1a7243c8104bbbcc24
mahdanidn <mahdanidn@gmail.com>
2023-03-24 05:33:53 +0700

    post add succesfuly

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Login from "./Login";
+import Logged from "./Logged";
 import { authOptions } from '../../pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth/next"
-import Logged from "./Logged";
 
 export default async function Nav() {
   const  session = await getServerSession(authOptions)
-  console.log(session);
+  // console.log(session);
   
   return (
     <nav className="flex justify-between items-center py-8">
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import AddPost from './components/AddPost'
+
 
 export default function Home() {
   return (
     <main>
         <h1 className="text-3xl font-bold underline">Hai Next</h1>
+        <AddPost />
     </main>
   )
 }
