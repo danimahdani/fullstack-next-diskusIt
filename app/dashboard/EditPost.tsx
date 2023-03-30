814cd2ad784c90ff734026195361adf82b43543c
mahdanidn <mahdanidn@gmail.com>
2023-03-31 06:32:30 +0700

    commit follow redirect

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -26,8 +26,19 @@ export default function EditPost({
 }: EditProps) {
   return (
     <div className="bg-white my-8 p-8 rounded-lg">
-      <Image src={avatar} alt="Avatar Image" width={32} height={32} />
-      <h3 className="font-bold text-gray-300">{name}</h3>
+      <div className="flex items-center gap-2">
+        <Image src={avatar} alt="Avatar Image" width={32} height={32} />
+        <h3 className="font-bold text-gray-300">{name}</h3>
+      </div>
+      <div className="my-8">
+        <p className="break-all">{title}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <p className="text-sm font-bold text-gray-700">
+          {comments?.length} Comments
+        </p>
+        <button className="text-sm font-bold text-red-500">Delete</button>
+      </div>
     </div>
   );
 }
